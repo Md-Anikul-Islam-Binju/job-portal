1b32be27804be39d717c1b30992f4148d41ea20a
Md Anikul Islam Binju <anikul.islam.binju@gmail.com>
2024-12-29 21:58:06 +0600

    update on

--- resources/views/user/app.blade.php
+++ resources/views/user/app.blade.php
@@ -188,7 +188,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12 text-center">
-                        <script>document.write(new Date().getFullYear())</script> © Powered By CoderNetix.</b>
+                        <script>document.write(new Date().getFullYear())</script> ©  Powered By  Bhawal Tech Ltd.</b>
                     </div>
                 </div>
             </div>
